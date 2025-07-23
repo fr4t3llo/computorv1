94b4a8bfc39a45e82656fff86e74be79aad5ca0e
FRATELLO <saife.addine123@gmail.com>
2025-07-23 21:39:47 +0100

    update by FRATELLO

--- src/main.cpp
+++ src/main.cpp
@@ -36,10 +36,11 @@ int main(int ac, char **av)
         cerr << "Error: equation must contain exactly one '=' sign." << endl;
         return 1;
     }
-    
+
 
     cout << "Left side: " << sides[0] << endl;
     cout << "Right side: " << sides[1] << endl;
 
+    
     return 0;
 }
